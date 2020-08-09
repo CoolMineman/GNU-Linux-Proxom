95a26d9c95188f204a284742029c2d5e16f490fb
tudorcioc3510 <tudorcioc@gmail.com>
2020-08-09 17:06:03 +0300

    Making the foreground service

--- app/src/main/java/com/tudor/android/proxom/ProxomService.java
+++ app/src/main/java/com/tudor/android/proxom/ProxomService.java
@@ -1,15 +1,22 @@
 package com.tudor.android.proxom;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class ProxomService extends Service {
     private MediaPlayer player;
@@ -25,6 +32,14 @@ public class ProxomService extends Service {
     private static volatile boolean broadcastingRunning = false;
 
     private Handler forceQuitHandler = null;
+
+    private Intent notificationIntent = null;
+    private PendingIntent pendingIntent = null;
+    private Notification notification = null;
+    private NotificationCompat.Builder notificationBuilder = null;
+
+    private final String CHANNEL_ID = "ProxomNotification";
+    private final int NOTIFICATION_ID = 1;
 
     static void setServerAddress(String serverAddress){
         ProxomService.serverAddress = serverAddress;
@@ -55,6 +70,39 @@ public class ProxomService extends Service {
         Toast.makeText(thisService, "Stopping broadcasting", Toast.LENGTH_SHORT).show();
     }
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Proxom Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    private void startNotification(){
+        createNotificationChannel();
+        notificationIntent = new Intent (thisService, MainActivity.class);
+        pendingIntent = PendingIntent.getActivity(thisService, 0, notificationIntent, 0);
+        notificationBuilder = new NotificationCompat.Builder(thisService, CHANNEL_ID);
+
+        notificationBuilder
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("Foreground service")
+                .setContentText("Haaatz")
+                .setContentIntent(pendingIntent);
+
+        notification = notificationBuilder.build();
+
+        startForeground(NOTIFICATION_ID, notification);
+
+
+    }
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -71,6 +119,8 @@ public class ProxomService extends Service {
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        startNotification();
+
         player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
         player.setLooping(true);
         player.start();
